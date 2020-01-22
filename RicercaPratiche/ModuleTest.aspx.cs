551166a1673a7f066cde4a408b368b2862c61a1c
sergio-villarreal <sergio.villarreal@regione.marche.it>
2020-01-22 14:42:28 +0100

    Caricamento immagini

--- RicercaPratiche/ModuleTest.aspx.cs
+++ RicercaPratiche/ModuleTest.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace RicercaPratiche
 {
 	public partial class ModuleTest : System.Web.UI.Page
-	{
+	{ 
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
